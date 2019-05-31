a9e44ebfa6a2fc5954428f6fc7a3f1fdc41d579e
AdmiringWorm <kim.nordmo@gmail.com>
2019-05-31 22:50:13 +0200

    (maint) Created utils index file

---
+++ src/utils/index.ts
@@ -0,0 +1,8 @@
+export { default as BaseGenerator } from "./base-generator";
+export { PathUtils } from "./file-utils";
+export {
+  GeneratorPrompts,
+  IOptionConfigEx,
+  PromptNames,
+} from "./generator-prompts";
+export { IGeneratorPrompt } from "./igenerator-prompt";
