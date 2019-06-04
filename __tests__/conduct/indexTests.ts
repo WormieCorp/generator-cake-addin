a06d6cec4af7f74605d53936a1bc2211b1aa449e
AdmiringWorm <kim.nordmo@gmail.com>
2019-06-04 02:54:06 +0200

    test(conduct): added failing unit test for new prompt
    - Added a new unit test before implementation of the email prompt
    
    ref #34

--- __tests__/conduct/indexTests.ts
+++ __tests__/conduct/indexTests.ts
@@ -3,9 +3,17 @@ import * as assert from "yeoman-assert";
 import * as helpers from "yeoman-test";
 
 describe("generator:conduct", () => {
-  beforeAll(() => helpers.run(join(__dirname, "../../src/conduct")));
+  beforeAll(() =>
+    helpers
+      .run(join(__dirname, "../../src/conduct"))
+      .withPrompts({ emailAddress: "kim.nordmo@gmail.com" })
+  );
 
   it("should create file", () => {
     assert.file("CODE_OF_CONDUCT.md");
+  });
+
+  it("should replace email address", () => {
+    assert.fileContent("CODE_OF_CONDUCT.md", /kim\.nordmo@gmail.com/);
   });
 });
