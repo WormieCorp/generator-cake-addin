5e1fb4c03765ca09ad5ec28dc4f08fcac061e4f3
AdmiringWorm <kim.nordmo@gmail.com>
2019-12-25 10:02:52 +0100

    chore(deps-dev): change commitlint to commitlint/cli

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: ["@commitlint/config-conventional", "@commitlint/config-angular"],
+  extends: ["@commitlint/config-conventional"],
   rules: {
     "body-max-line-length": [2, "always", 100],
     "footer-max-line-length": [2, "always", 100],
